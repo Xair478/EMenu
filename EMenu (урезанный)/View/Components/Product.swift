f48cf90fb2b8af290570a35479c2722dcc636758
Xair478 <abikaamantaev820@gmail.com>
2022-10-14 07:10:30 +0600

    homePagePreparation v0.1

---
+++ EMenu (урезанный)/View/Components/Product.swift
@@ -0,0 +1,24 @@
+//
+//  Product.swift
+//  EMenu (урезанный)
+//
+//  Created by Абылхаир Амантаев on 14.10.2022.
+//
+
+import Foundation
+import UIKit
+
+struct Product {
+    
+    var id: String
+    var title: String
+    var imageUrl: String
+    var price: Int
+    var description: String
+    //var ordersCount: Int
+    //var recomend: Bool
+    
+    
+    
+    
+}
